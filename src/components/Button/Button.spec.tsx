f78ee67a51db3e65335107cfad9939085108b75a
Bernardo Borges <bernardobbm334@gmail.com>
2023-01-27 14:05:05 -0300

    added snapshot file for Button component tests

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -2,10 +2,10 @@ import { describe, expect, it, vitest } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { Button } from '.';
 
+const fn = vitest.fn();
+
 describe('Button Component', () => {
 	it('should render the button with the text', () => {
-		const fn = vitest.fn();
-
 		render(
 			<Button
 				onClick={fn}
@@ -19,8 +19,6 @@ describe('Button Component', () => {
 	});
 
 	it('should call a function on button click', () => {
-		const fn = vitest.fn();
-
 		render(
 			<Button
 				onClick={fn}
@@ -36,8 +34,6 @@ describe('Button Component', () => {
 	});
 
 	it('should be disabled when disable prop is true', () => {
-		const fn = vitest.fn();
-
 		render(
 			<Button
 				onClick={fn}
@@ -49,5 +45,17 @@ describe('Button Component', () => {
 		const button = screen.getByRole('button', { name: /load more/i });
 
 		expect(button).toBeDisabled();
+	});
+
+	it('should match snapshot', () => {
+		const { container } = render(
+			<Button
+				onClick={fn}
+				disabled={true}
+				text="load more"
+			/>
+		);
+
+		expect(container.firstChild).toMatchSnapshot();
 	});
 });
